bf86e5c4d8487a7fc4fad77c5cee2c077a680df3
Christian Ward <christianward97@outlook.com>
2022-06-14 11:15:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+  const [newExcuse, setExcuse] = useState([]);
+  const [error, setError] = useState (null);
+
+useEffect(()=>{
+  const fetchData = async ()=>{
+    try {
+      const response = await fetch
+      ('https://excuser.herokuapp.com/v1/excuse/office/3');
+      console.log (response)
+      if(!response.ok){
+        throw new Error(response.statusText)
+      }
+      const data = await response.json();
+      setExcuse(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      setError('Could not fetch the data');
+    }
+  };
+  fetchData();
+},[])
+
+return (
+<div className="App">
+<div>
+<h1>Excuses API</h1>
+{error && <p>{error}</p>}
+{newExcuse.map ((excuse)=>(
+<div key={excuse.id}>
+  <h3>{excuse.excuse}</h3>
+
+    </div>
+
+))}
+</div>
+</div>
+);
+};
 export default App;
